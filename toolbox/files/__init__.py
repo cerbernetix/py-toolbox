8ffe204ceb798af64d55d1e0d0db16e5528729ea
jsconan <jsconan@gmail.com>
2023-09-09 14:07:17 +0200

    feat: add a class for reading and writing JSON files

--- toolbox/files/__init__.py
+++ toolbox/files/__init__.py
@@ -3,6 +3,13 @@ Entry point for the `files` package.
 """
 from toolbox.files.file import get_file_mode, read_file, write_file
 from toolbox.files.file_manager import FileManager
+from toolbox.files.json_file import (
+    JSON_ENCODING,
+    JSON_INDENT,
+    JSONFile,
+    read_json_file,
+    write_json_file,
+)
 from toolbox.files.path import (
     create_file_path,
     delete_path,
