c1e66814a3a5d4357b7aaccf32c8662abbd1b849
jsconan <jsconan@gmail.com>
2023-09-10 22:30:57 +0200

    doc: add summary for the main package

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,3 +1,1 @@
-"""
-`py-toolbox` is a collection of utilities offering solutions to a set of common problems.
-"""
+"""`py-toolbox` is a collection of utilities offering solutions to a set of common problems."""
