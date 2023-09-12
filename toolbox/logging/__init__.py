853225e45cbaed0373c3c1827e69f80a12c570ba
jsconan <jsconan@gmail.com>
2023-09-12 11:10:35 +0200

    feat: add a class for logging to a custom file

--- toolbox/logging/__init__.py
+++ toolbox/logging/__init__.py
@@ -4,18 +4,33 @@ It contains:
 - Logging config:
     - `setup_file_logging()` - Setup file logging for the application.
     - `handle_uncaught_exceptions()` - Installs a collector for logging uncaught exceptions.
+- File logger:
+    - `LogFile(filename, ...)` - A custom logger that writes directly to a file.
 
 Examples:
 ```python
 import logging
 
-from toolbox.logging import setup_file_logging, handle_uncaught_exceptions
+from toolbox.logging import LogFile, setup_file_logging, handle_uncaught_exceptions
 
 logging.setup_file_logging('path/to/app.log')
 logging.handle_uncaught_exceptions()
 
+logger = logging.getLogger(__name__)
+
+def action(log_file: LogFile) -> None:
+    value = input('Please enter a value: ')
+
+    log_file.info('The received value is %s', value)
+
 def main() -> None:
-    ...
+    logger.info('Program started')
+
+    log_file = logging.LogFile('path/to/action.log')
+
+    action(log_file)
+
+    logger.info('Program ended')
 
 if __name__ == "__main__":
     main()
@@ -27,4 +42,10 @@ from toolbox.logging.config import (
     LOG_FORMAT,
     handle_uncaught_exceptions,
     setup_file_logging,
+)
+from toolbox.logging.log_file import (
+    LOG_FILE_LOG_FORMAT,
+    LOG_FILE_NAME,
+    LOG_FILE_PATTERN,
+    LogFile,
 )
